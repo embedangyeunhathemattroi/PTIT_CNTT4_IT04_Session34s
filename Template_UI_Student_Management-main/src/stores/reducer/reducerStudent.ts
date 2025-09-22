2e798dc0eaf1e1b52a0f5ef0d33b6efe6715f002
helloPTIT <phamngoclinhtq2006@gmail.com>
2025-09-22 23:41:51 +0700

    Update reducerStudent.ts

--- Template_UI_Student_Management-main/src/stores/reducer/reducerStudent.ts
+++ Template_UI_Student_Management-main/src/stores/reducer/reducerStudent.ts
@@ -1,47 +1,51 @@
-const initialState = {
-  users: [
-    {
-      id: "SV001",
-      name: "Nguyễn Văn A",
-      age: 20,
-      gender: "Nam",
-      birthday: "2025-11-11",
-      hometown: "Hà Nội",
-      address: "Hà Nội",
-    },
-    {
-      id: "SV002",
-      name: "Nguyễn Văn B",
-      age: 21,
-      gender: "Nữ",
-      birthday: "2025-11-11",
-      hometown: "Hà Nội",
-      address: "Hà Nội",
-    },
-    {
-      id: "SV003",
-      name: "Nguyễn Văn C",
-      age: 19,
-      gender: "Nam",
-      birthday: "2025-11-11",
-      hometown: "Hà Nội",
-      address: "Hà Nội",
-    },
-  ],
-};
+import type { Student } from "../../utils/types";
 
-export const reducerStudent = (state = initialState, action: any) => {
+const initialState: Student[] = [
+  {
+    id: "SV001",
+    name: "Nguyễn Văn A",
+    age: 20,
+    gender: "Nam",
+    birthday: "2025-11-11",
+    hometown: "Hà Nội",
+    address: "Hà Nội",
+  },
+  {
+    id: "SV002",
+    name: "Nguyễn Văn B",
+    age: 21,
+    gender: "Nữ",
+    birthday: "2025-11-11",
+    hometown: "Hà Nội",
+    address: "Hà Nội",
+  },
+  {
+    id: "SV003",
+    name: "Nguyễn Văn C",
+    age: 19,
+    gender: "Nam",
+    birthday: "2025-11-11",
+    hometown: "Hà Nội",
+    address: "Hà Nội",
+  },
+];
+
+export const reducerStudent = (
+  state = initialState,
+  action: any
+): Student[] => {
   switch (action.type) {
     case "ADD":
-      return state;
-    case "WATCH":
-      return state;
+      return [...state, action.payload];
+
     case "EDIT":
-      return state;
+      return state.map((stu) =>
+        stu.id === action.payload.id ? { ...stu, ...action.payload } : stu
+      );
+
     case "DELETE":
-      return state;
-    case "SEARCH":
-      return state;
+      return state.filter((stu) => stu.id !== action.payload);
+
     default:
       return state;
   }
